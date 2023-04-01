a024222c9727182a2024917a5be636a9d934b563
Ivanov Pavel <exipse@yandex.ru>
2023-04-01 17:43:37 +0300

    T3 -11. refactor check-repo2

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -48,12 +48,12 @@ public class UserDbStorage implements UserStorage {
         String sqlQuery = "update users set " +
                 "email = ?, login = ?, name = ?, birthday = ? " +
                 "where id = ?";
-        jdbcTemplate.update(sqlQuery
-                , user.getEmail()
-                , user.getLogin()
-                , user.getName()
-                , user.getBirthday()
-                , user.getId());
+        jdbcTemplate.update(sqlQuery,
+                user.getEmail(),
+                user.getLogin(),
+                user.getName(),
+                user.getBirthday(),
+                user.getId());
 
         return user;
     }
